bc859bd86dccca3a4f828f0bf370c303a3ed5ad6
Yuliya Pauzunova <julia.polzunova@icloud.com>
2025-08-17 15:37:37 +0100

    Update 04_Neeps Hard Questions.sql
    Neeps Hard Question 12

--- 04_Neeps Hard Questions.sql
+++ 04_Neeps Hard Questions.sql
@@ -43,4 +43,83 @@ from staff_timetable st1 join staff_timetable st2
 		or 
 		st2.tod_end > st1.tod_start and st2.tod_end <= st1.tod_end
 	)
+
 order by st1.week, st1.dow, st1.tod_start, st1.duration, st2.tod_start
+
+
+
+
+--- 12. Produce a table showing the utilisation rate and the occupancy level 
+---     for all rooms with a capacity more than 60.
+
+	
+-- Definitions:                  
+-- Utilisation rate = (total hours used during the semester) / (total available hours in semester)
+--     				where total available hours in semester (900) = weeks per semester (15) * days per week (5) * hours per day (12) 
+-- Occupancy level = (duration-weighted average number of students per event) / (room capacity)
+
+	
+-- Step 1: Calculate total available hours in semester
+with semester_weeks as (
+	select count(distinct id) as weeks_per_semester from week
+)
+
+, week_days as (
+	select count(distinct dow) as days_per_week from event
+)
+	
+-- Assume all rooms have the same daily span - select max 
+, day_hours as (
+	select max(hours_per_day) as max_hours_per_day 
+	from (
+		select 
+			room, 
+			max(tod + duration) - min(tod) as hours_per_day
+		from event
+		group by room
+	) as hours_room
+) 
+
+, availability as (
+	select
+		*, 
+		cw.weeks_per_semester * cd.days_per_week * ch.max_hours_per_day as  semester_hours
+	from semester_weeks cw join week_days cd join day_hours ch
+)
+
+-- Step 2: Build timetable of all distinct event occurrences, 
+--         including duration and actual seats occupancy
+, events_timetable as (
+	select 
+		e.id, 
+		e.room, 
+		o.week, 
+		e.dow, 
+		e.tod, 
+		e.duration,   
+		sum(s.sze) as room_occupancy 
+	from event e 
+		join attends a on e.id = a.event
+		join student s on a.student = s.id
+		join occurs o on e.id = o.event
+	group by e.room, o.week, e.dow, e.tod, e.duration, e.id
+)
+
+-- Step 3: For each room with capacity > 60, calculate:
+--         - Utilisation rate = total duration / semester availability
+--         - Occupancy level  = duration-weighted avg occupancy / capacity
+, summary as (
+	select 
+		room, 
+		capacity, 
+		sum(duration) / semester_hours as utilisation_rate,
+		(sum(room_occupancy * duration) / sum(duration))/ max(capacity) as occupancy_level
+	from events_timetable e 
+		cross join availability
+		join room on e.room = room.id
+	where capacity > 60
+	group by room, capacity 
+)
+
+-- Step 4: Return final summary table
+select * from summary
