2c254f855c2f3159bc12fb4e86d396b437feefe5
Yuliya Pauzunova <julia.polzunova@icloud.com>
2025-08-23 09:10:05 +0100

    Update 05_Musicians Hard Questions.sql
    Musicians Hard Question 12

--- 05_Musicians Hard Questions.sql
+++ 05_Musicians Hard Questions.sql
@@ -16,3 +16,26 @@ left join musician jf
 	on jf.m_name = 'James First'
 where m.born_in = jf.born_in
 and m.m_name != 'James First' 
+
+
+
+
+--- 12. Create a list showing for EVERY musician born in Britain 
+---     the number of compositions and the number of instruments played.
+
+-- left join to list EVERY musician (incl. with zero-counts)
+select 
+	m.m_name as musician
+	, count(distinct cc.cmpn_no) as compositions_num
+	, count(distinct pfmr.instrument) as instruments_num
+
+from musician m 
+left join place p 			on m.born_in 		= p.place_no
+left join composer cmpr 	on m.m_no 			= cmpr.comp_is
+left join has_composed cc 	on cmpr.comp_no 	= cc.cmpr_no
+left join performer pfmr 	on m.m_no 			= pfmr.perf_is
+
+where p.place_country in ('England', 'Scotland')
+group by m.m_name
+
+order by 1
