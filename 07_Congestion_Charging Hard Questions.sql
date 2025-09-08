cc2a08424ce35de24972f79ca83eee6ed717abd3
Yuliya Pauzunova <julia.polzunova@icloud.com>
2025-09-08 12:26:44 +0100

    Update 07_Congestion_Charging Hard Questions.sql
    Congestion_Charging Hard 2

--- 07_Congestion_Charging Hard Questions.sql
+++ 07_Congestion_Charging Hard Questions.sql
@@ -1,4 +1,16 @@
 ---- Congestion Charging Hard Questions
 ---- Schema & Tasks Link: https://sqlzoo.net/wiki/Congestion_Charging
 
---- 11. 
+
+--- 2. There are four types of permit. The most popular type means that 
+---    this type has been issued the highest number of times. 
+---    Find out the most popular type, together with the total number of permits issued.
+
+
+select 
+	chargeType 	as permit_type, 
+	count(*) 	as times_issued
+from permit 
+group by chargeType
+order by 2 desc
+fetch next 1 row with ties 
